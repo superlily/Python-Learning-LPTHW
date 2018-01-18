62cd3cdfb2ff879f46c07492c60daa82f31d9e24
superlilygogogo <superlily121@gmail.com>
2018-01-18 12:46:09 +0800

    Upload pyex3.py

---
+++ pyex3.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+print "I will now count my chickens(我要开始数鸡啦):"
+
+print "Hens", 25 + 30/6 #The answer is 30
+print "Roosters", 100 - 25*3 % 4 #The answer is 97
+
+print "Now I will count the eggs(我要开始数鸡蛋了)："
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2 < 5-7?"
+
+print 3+2 < 5-7
+
+print "What is 3+2.55?", 3+2
+
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+print "What is 7/4?", 7.0/4.0
